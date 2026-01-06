2d639cab58ccaaa3e6eab64ffaff91c3743c0d03
Yashi Sharma <yashitechai@gmail.com>
2026-01-06 09:28:07 +0530

    updated quick sort

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -1,5 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+int partition(vector <int> &arr,int low,int high ){
+    
+}
+vector <int> quick_sort(vector <int> &arr, int low , int high){
+    if(low < high){
+        int pindex = partition(arr,low,high);
+        quick_sort(arr,low,pindex-1);
+        quick_sort(arr,pindex + 1, high);
+    }
+
+}
 int main(){
     int n ;
     cout << "Enter the number of elements : " << endl;
