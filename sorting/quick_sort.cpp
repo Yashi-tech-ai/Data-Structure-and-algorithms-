3e2fcd0528501677dd61ee3555497381b0e500e4
Yashi Sharma <yashitechai@gmail.com>
2026-01-06 09:42:45 +0530

    completed quick sort

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -5,15 +5,21 @@ int partition(vector <int> &arr,int low,int high ){
     int i = low;
     int j = high;
     while(i<j){
-        while(arr[i]<= arr[pivot] && i <=high){
+        while(arr[i]<= pivot && i <=high -1){
             i++;
         }
-        while(arr[j]  >= arr[pivot] && j >= low){
+        while(arr[j]  > pivot && j >= low +1 ){
             j--;
         }
+        if(i<j){
+            swap(arr[i],arr[j]);
+        }
+
     }
+        swap(arr[low],arr[j]);
+        return j;
 }
-vector <int> quick_sort(vector <int> &arr, int low , int high){
+void quick_sort(vector <int> &arr, int low , int high){
     if(low < high){
         int pindex = partition(arr,low,high);
         quick_sort(arr,low,pindex-1);
