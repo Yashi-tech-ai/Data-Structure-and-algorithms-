3624842f81155d8411096e890da8d01b99828b93
Yashi Sharma <yashitechai@gmail.com>
2026-01-07 09:52:49 +0530

    questions on array

--- Arrays/arrays_easy.cpp
+++ Arrays/arrays_easy.cpp
@@ -15,9 +15,26 @@ int largestelement(int arr[], int n){
 
 }
 
+// second largest without sorting 
+int seclargest(int arr[], int n){
+    int largest = arr[0];
+    int seclarge = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] > largest){
+            seclarge = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] < largest && arr[i] > seclarge){
+            seclarge = arr[i];
+        }
+    }
+    return seclarge ;   
+}
+
 
 int main(){
    int arr[5] = {3,2,1,5,2};
-   cout << largestelement(arr,5);
+   cout << seclargest(arr,5);
   return 0;
 }
