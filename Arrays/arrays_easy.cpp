6462c4c5f2b440165ff7c7bb387354a2a7ee3efe
Yashi Sharma <yashitechai@gmail.com>
2026-01-17 16:11:51 +0530

    leetcode 485

--- Arrays/arrays_easy.cpp
+++ Arrays/arrays_easy.cpp
@@ -148,6 +148,24 @@ public:
 };*/
 
 
+// leetcode 485
+// brute = count and incremenet and then compare {tc = O(N)}
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi = 0; 
+        int cnt = 0;
+        for(int i = 0;i < nums.size();i++){
+            if(nums[i] == 1){
+                cnt++;
+                maxi = max(maxi,cnt);
+            }
+            else cnt = 0;
+        }
+
+        return maxi;
+    }
+};
 
 int main(){
    int arr[5] = {3,2,1,5,2};
