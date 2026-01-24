b0c115f49ed14681860d0a6d7eeddd7b31d56b18
Yashi Sharma <yashitechai@gmail.com>
2026-01-24 17:51:18 +0530

    leetcode problem  35

--- binary search/bs_easy.cpp
+++ binary search/bs_easy.cpp
@@ -1,2 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+// leetcode = 35
+// lower bound 
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2; // safer to avoid overflow
+            if(nums[mid] == target) {
+                return mid; // found target
+            } else if(nums[mid] < target) {
+                low = mid + 1; // go right
+            } else {
+                high = mid - 1; // go left
+            }
+        }
+
+        // If not found, 'low' is the correct insert position
+        return low;
+    }
+};
