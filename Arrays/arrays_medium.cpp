62ee46bf1b92ff1b9f72d39656d3e3dc24ed0123
Yashi Sharma <yashitechai@gmail.com>
2026-02-01 08:34:55 +0530

    optimal approach to the problem

--- Arrays/arrays_medium.cpp
+++ Arrays/arrays_medium.cpp
@@ -277,4 +277,28 @@ vector<int> nextgreaterpermutation(vector<int> &arr){
    next_permutation(arr.begin(),arr.end());
    return arr;
 }
-// optimal = 
+// optimal = first find a break through point then find the number just bigger than that index and then reverse the right array from indec = {O(3N)}
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int ind = -1;
+        int n = nums.size();
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+        if(ind == -1){
+           reverse(nums.begin(),nums.end());
+           return ;
+        }
+        for(int i = n-1;i > ind ;i--){
+            if(nums[i] > nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin()+ ind + 1, nums.end());
+    }
+};
