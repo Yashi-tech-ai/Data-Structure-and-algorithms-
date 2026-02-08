d1656f6aacea0a16430e0e68b5dbba5c4d2e221f
Yashi Sharma <yashitechai@gmail.com>
2026-02-08 14:07:20 +0530

    optimal approach ti find single element in an sorted array

--- binary search/bs_easy.cpp
+++ binary search/bs_easy.cpp
@@ -266,7 +266,32 @@ public:
 
 // leetcode 540 = Single element in an sorted array 
 // brute = check element one by one and their left and right side , by XOR , by Map = {O(N)}
+// optimal = by observation on left of the single element (even,odd) and right of the element (odd,even) this will help to eliminate
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        int low = 1; int high = n-2;
+        while(low <= high){
+            int mid = low + (high - low)/2 ;
+            if(nums[mid]!= nums[mid +1] && nums[mid] != nums[mid-1]){
+                return nums[mid];
+            }
+            // we are in left 
+            if((mid % 2 == 1) && nums[mid] == nums[mid -1] || (mid % 2 == 0) && nums[mid] == nums[mid + 1]){
+                low = mid + 1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return -1;
 
+    }
+};
 
 
 
